1b64a6e18a702e4b9a9ece5475a0025a8686c6a0
Andriy Yurchuk <ayurchuk@minuteware.net>
2014-09-02 14:45:07 +0300

    Refactor. Prepare for PyPi

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+from mailhole import __version__
+
+
+setup(
+    name = 'mailhole',
+    version=__version__,
+    description = 'Fake SMTP server, used for testing',
+    author = 'Andriy Yurchuk',
+    author_email = 'ayurchuk@minuteware.net',
+    url = 'https://github.com/Ch00k/mailhole',
+    license = 'LICENSE.txt',
+    long_description = open('README.rst').read(),
+    entry_points = {
+      'console_scripts': [
+          'mailhole = mailhole.mailhole:main'
+      ]
+    },
+    packages = ['mailhole'],
+    data_files = [('/etc/mailhole/', ['data/mailhole.conf'])]
+)
